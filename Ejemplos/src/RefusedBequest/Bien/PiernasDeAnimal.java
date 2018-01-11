971c83a865a2e88d934b4d80c68ae1ec7c752fdd
llullanqui <luis.1142@hotmail.com>
2018-01-10 19:58:12 -0500

    Refused Bequest

---
+++ Ejemplos/src/RefusedBequest/Bien/PiernasDeAnimal.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RefusedBequest.Bien;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PiernasDeAnimal {
+    
+    public void correr(){
+        //Comandos para correr
+    }
+    
+    public void saltar(){
+        //Comandos para saltar
+    }
+    
+    public void caminar(){
+        //Comandos para caminar
+    }
+    
+    public void acuclillarse(){
+        //Comandos para acuclillarse
+    }
+    
+    public void arrodillarse(){
+        //Comandos para arrodillarse
+    }
+    
+    public void irDeLado(){
+        //Comandos para ir de lado como el cangrejo :v
+    }
+    
+    public void caminarHaciaAtras(){
+        //Comandos para caminar hacia atras
+    }
+    
+}
---
+++ Ejemplos/src/RefusedBequest/Mal/Rana.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RefusedBequest.Mal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Rana extends PiernasDeAnimal {
+    
+    @Override
+    public void saltar(){
+        //Comandos para hacer que la rana salte.
+    }
+    
+    //No utiliza ningun otro metodo porque la Rana no sabe hacer mas con sus piernas.
+
+    @Override
+    public void correr() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void caminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void acuclillarse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void arrodillarse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void irDeLado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void caminarHaciaAtras() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
